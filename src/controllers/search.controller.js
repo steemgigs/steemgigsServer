efe06ac9c3e902f17484b86054d0bb058906e789
Tobias <tobias.gooney@icloud.com>
2019-02-09 23:47:26 +0000

    Simplify search request foundations

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -1,35 +1,22 @@
 const Post = require('../model/post')
-const {stringify, handleErr} = require('../utils')
+const {handleErr} = require('../utils')
 
 exports.search = (req, res) => {
-  let {query} = req.body
-  let limit = req.params.limit || 50
-  if (query) {
-    console.log('query:', stringify(query))
-    let {searchText} = query
-    console.log(searchText, Boolean(searchText))
-    if (searchText.trim().length === 0 || searchText.trim() === '.' || searchText.trim().toLowerCase() === 'steemgigs') {
-      Post.find({type: 'steemgigs_post'}).select('').limit(limit).exec((err, result) => {
-        if (!err) {
-          console.log(JSON.stringify(result, null, 2))
-          res.status(200).send(result)
-        } else {
-          handleErr(err, res, 'empty result')
-        }
-      })
-    } else if (searchText.trim().length > 0) {
-      Post.find({type: 'steemgigs_post', $text: {$search: searchText.trim()}}, { score: {$meta: 'textScore'} }).select('').limit(limit).exec((err, result) => {
-        if (!err) {
-          console.log(JSON.stringify(result, null, 2))
-          res.status(200).send(result)
-        } else {
-          handleErr(err, res, 'empty result')
-        }
-      })
-    } else {
-      res.status(400).send('please supply a proper query')
-    }
-  } else {
-    res.status(400).send('please supply a proper query')
+  const {query} = req.body
+  const limit = req.params.limit || 50
+  let searchResult = []
+  try {
+    var {searchText} = query
+    Post.find({type: 'steemgigs_post', $text: {$search: searchText.trim()}}, { score: {$meta: 'textScore'} }).select('').limit(limit).exec((err, result) => {
+      if (!err) {
+        searchResult = result
+      } else {
+        handleErr(err, res, 'empty result')
+      }
+      res.status(200).send(searchResult)
+    })
+  } catch (err) {
+    res.status(500)
+    console.log(err)
   }
 }

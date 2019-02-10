87c2fa507476065eb67ab0e20ac7481fb827fcf8
Tobias <tobias.gooney@icloud.com>
2019-02-10 02:33:01 +0000

    Convery search to aggregate & add pagination

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -2,14 +2,31 @@ const Post = require('../model/post')
 const {handleErr} = require('../utils')
 
 exports.search = (req, res) => {
-  const {searchText, type, currency} = req.body.query
-  const limit = req.params.limit || 50
-
-  let searchResult = []
+  const {searchText, type, currency, pageNumber, limit} = req.body.query
+  const skipCount = pageNumber * limit
+  let searchResult = {}
   try {
-    Post.find({type: type, $text: {$search: searchText.trim()}, currency: currency}, { score: {$meta: 'textScore'} }).select('').limit(limit).exec((err, result) => {
+    Post.aggregate([
+      { $match: {$text: {$search: searchText.trim()}} },
+      { $addFields: {score: {$meta: 'textScore'}} },
+      { '$facet': {
+        'search_data': [
+          {'$match': {type: type}},
+          {'$match': {currency: currency}},
+          {'$skip': skipCount},
+          {'$limit': limit}
+        ],
+        'post_count': [
+          {'$match': {type: type}},
+          {'$match': {currency: currency}},
+          { $count: 'count' }
+        ]
+      }}
+    ]).exec((err, result) => {
       if (!err) {
-        searchResult = result
+        // Add results to search result object and calculate total number of pages available for client side UI
+        searchResult.results = result[0].search_data
+        searchResult.pages = result[0].post_count[0].count / limit
       } else {
         handleErr(err, res, 'empty result')
       }
